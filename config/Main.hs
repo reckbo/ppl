1d622154bd5d4181bdbf2962b6e57c41ef928bad
reckbo <reckbo@bwh.harvard.edu>
2016-12-13 11:27:27 +1100

    ENH: Add commandline case id targets

--- config/Main.hs
+++ config/Main.hs
@@ -3,15 +3,24 @@ import           Node
 import           Node.MeasureTractsCsv    hiding (rules)
 import           Paths                    (outdir)
 import           Shake.BuildNode
+import           System.Console.GetOpt
+-- import Data.Maybe ( fromMaybe )
+
+
+-- options :: [OptDescr String-- ]
+-- options = [
+--      Option ['l']  ["caselist"]  (OptArg (fromMaybe "stdin") "FILE")  "output FILE"
+--      ]
 
 main :: IO ()
-main = shakeArgs shakeOptions{shakeFiles=outdir
-                             ,shakeVerbosity=Chatty
-                             ,shakeTimings=True} $ do
+-- main = shakeArgs shakeOptions{shakeFiles=outdir ,shakeVerbosity=Chatty} $ do
+main = shakeArgsWith shakeOptions{shakeFiles=outdir,shakeVerbosity=Chatty} [] $ \_ targets -> return $ Just $ do
   usingConfigFile "config/settings.cfg"
 
   action $ do
-    caseids <- readFileLines "config/caselist.txt"
+    caseids <- if null targets
+               then readFileLines "config/caselist.txt"
+               else return targets
     let nodes = [
           MeasureTractsCsv {fstype=FreeSurferWithMask StructuralMaskMabs
                            ,fs2dwitype=FsBrain_B0
