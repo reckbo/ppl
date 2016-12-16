8aba98c67881110a24fae38014eb292885358759
reckbo <reckbo@bwh.harvard.edu>
2016-12-16 16:46:23 +1100

    STY: remove FreeSurfer lib module

--- pipeline-lib/Node/FreeSurfer.hs
+++ pipeline-lib/Node/FreeSurfer.hs
@@ -8,11 +8,18 @@ module Node.FreeSurfer
   ) where
 
 import           Data.Maybe
-import qualified FreeSurfer          (runWithMask)
 import           Node.Structural     hiding (rules)
 import           Node.StructuralMask hiding (rules)
 import           Node.Util
 import           Shake.BuildNode
+import qualified System.Directory         as IO
+import           System.Environment       (getEnvironment, lookupEnv)
+import           Util                     (convertImage, maskImage)
+import           FSL                      (isNifti)
+import           Data.List                (intercalate)
+import           Data.List.Split
+import           Control.Monad            (when)
+import           Control.Monad.Extra      (unlessM, whenM)
 
 type CaseId = String
 
@@ -39,10 +46,66 @@ instance BuildNode FreeSurfer where
     let mask = StructuralMask (masktype, T1w, caseid)
     need mask
     need strct
-    FreeSurfer.runWithMask
+    runWithMask
       [5,3,0]
       (path mask)
       (path strct)
       (takeDirectory . pathDir $ n)
 
 rules = rule (buildNode :: FreeSurfer -> Maybe (Action [Double]))
+
+--------------------------------------------------------------------------------
+-- Functions
+
+type Version = [Int]
+
+assertVersion :: Version -> Action FilePath
+assertVersion requiredVersion =
+  let showVersion = intercalate "." . map show
+      extractVersion = map read . splitOn "." . tail . head . reverse . splitOn "-"
+  in do
+    fshome <- liftIO $ (fromMaybe $ error "Set FREESURFER_HOME") <$> lookupEnv "FREESURFER_HOME"
+    let versionFile = fshome </> "build-stamp.txt"
+    exists <- liftIO $ IO.doesFileExist versionFile
+    when (not exists)
+      (error $ "Can't check Freesurfer version, "
+       ++ fshome ++ "/build-stamp.txt doesn't exist. Is that directory correct?")
+    version <- liftIO $ extractVersion <$> readFile versionFile
+    when (version /= requiredVersion) $
+        error $ "Freesurfer version " ++ (showVersion version)
+                ++ " detected, but require " ++ (showVersion requiredVersion)
+    return fshome
+
+run :: Version -> Bool -> FilePath -> FilePath -> Action ()
+run version skullstrip t1 outdir = withTempDir $ \tmpdir -> do
+  liftIO $ unlessM (IO.doesFileExist t1)
+    (error $ "Freesurfer: run: "++t1++" does not exist")
+  fshome <- assertVersion version
+  let caseid       = dropExtensions . takeBaseName $ t1
+      subjectsDir  = tmpdir </> "subjects"
+      fsdir        = subjectsDir </> caseid
+      t1mgz        = fsdir </> "mri" </> "T1.mgz"
+      brainmaskmgz = fsdir </> "mri" </> "brainmask.mgz"
+  liftIO $ IO.createDirectoryIfMissing False subjectsDir
+  t1nii <- liftIO $ if isNifti t1 then return t1
+                    else (do Util.convertImage t1 (tmpdir </> "t1.nii.gz")
+                             return (tmpdir </> "t1.nii.gz"))
+  fscmd fshome subjectsDir "recon-all" $ ["-s", caseid ,"-i", t1nii ,"-autorecon1"]
+    ++ (if skullstrip then [] else ["-noskullstrip"])
+  liftIO $ IO.copyFile t1mgz brainmaskmgz
+  fscmd fshome subjectsDir "recon-all" ["-autorecon2", "-subjid", caseid]
+  fscmd fshome subjectsDir "recon-all" ["-autorecon3", "-subjid", caseid]
+  liftIO $ whenM (IO.doesDirectoryExist outdir) (IO.removeDirectoryRecursive outdir)
+  unit $ cmd "mv" fsdir outdir
+
+runWithMask :: Version -> FilePath -> FilePath -> FilePath -> Action ()
+runWithMask version mask t1 outdir = withTempDir $ \tmpdir -> do
+    let maskedt1 = tmpdir </> "masked-" ++ (takeBaseName t1)
+    liftIO $ Util.maskImage t1 mask maskedt1
+    run version False maskedt1 outdir
+
+fscmd :: FilePath -> FilePath -> FilePath -> [FilePath] -> Action ()
+fscmd fshome subjdir exe args
+  = cmd Shell "source" (fshome </> "SetUpFreeSurfer.sh")
+    ("; export SUBJECTS_DIR=" ++ subjdir)
+    ";" exe args
