fb651be6795edee42b93693b6fb94f159470b28d
reckbo <reckbo@bwh.harvard.edu>
2016-12-16 11:36:59 +1100

    STY/BUG: Make shake database save to outdir

--- Main.hs
+++ Main.hs
@@ -5,7 +5,8 @@ import           Shake.BuildNode
 import           Need
 
 main :: IO ()
-main = shakeArgsWith shakeOptions{shakeVerbosity=Chatty
+main = shakeArgsWith shakeOptions{shakeFiles=Paths.outdir
+                                 ,shakeVerbosity=Chatty
                                  ,shakeReport=["report.html","report.json"]} [] 
                                  $ \_ targets -> return $ Just $ do
   usingConfigFile "config/settings.cfg"
